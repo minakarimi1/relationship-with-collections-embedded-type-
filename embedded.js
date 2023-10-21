dc6384bf9512cd4938052187eb144640a91d1852
minakarimi1 <minakarimi1@yahoo.com>
2023-10-21 21:04:12 +0330

    added updateuser

--- embedded.js
+++ embedded.js
@@ -4,16 +4,18 @@ const app = express();
 import mongoose from "mongoose";
 const port = process.env.PORT||3000;
 
+//>>> create embeddedDB collection <<<
 mongoose.connect('mongodb://127.0.0.1:27017/embeddedDB')
 .then(()=>console.log('connected to mongo DB'))
 .catch((err) => console.log("could not connect to DB", err));
 
+//>>> create Books and User document in DB<<<
 const bookSchema = new mongoose.Schema({
   title: {type: String},
   pages: {type: Number},
-})
-
+});
 const Books = mongoose.model('Books', bookSchema);
+
 
 const Users =mongoose.model('Users', new mongoose.Schema({
   first_name: { type: String },
@@ -21,7 +23,7 @@ const Users =mongoose.model('Users', new mongoose.Schema({
   book: bookSchema,
 }));
 
-
+//>>> create function from add new user <<<
 async function createUser(first_name,last_name,book){
   const users = new Users({
     first_name,
@@ -31,14 +33,29 @@ async function createUser(first_name,last_name,book){
   const result = await users.save();
   console.log(result);
 };
-createUser('mina','karimi',new Books({title:'nodejs programing', pages: 150}));
+// createUser('mina','karimi',new Books({title:'nodejs programing', pages: 150}));
 
+
+//>>> update user <<<
+async function updateUser(id){
+  await Users.updateMany({_id:id},{
+    $set : {
+      'book.title': 'nodejs',
+      'book.pages': 100,
+    }
+  })
+}
+updateUser('6533fed328e0860fdae1eb8c');
+
+
+//>>> get user <<<
 async function getUser(){
   const result = await Users.find()
   console.log(result);
 }
-
 getUser();
+
+
 
 app.listen(port,()=>{
   console.log(`server run on port: ${port}`);
